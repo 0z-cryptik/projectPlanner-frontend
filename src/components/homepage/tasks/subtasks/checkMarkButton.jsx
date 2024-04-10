b69406ff1a1a626cef22e2ad4785a3bab3567dda
Eniitan <enitanolawale0@gmail.com>
2024-04-10 11:06:53 +0100

    checkbox set

---
+++ src/components/homepage/tasks/subtasks/checkMarkButton.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useList } from "../../../../hooks/stateProvider";
+import { IoIosCheckmark } from "react-icons/io";
+
+export const CheckMarkButton = ({ clickFunc }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <button
+      className="mr-4 border rounded-full"
+      onClick={clickFunc}
+      onMouseOver={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}>
+      <IoIosCheckmark
+        className={`${!hover && "opacity-0"}`}
+        size={"1.3rem"}
+      />
+    </button>
+  );
+};
