d5b961aaa434a4bb89c457825953cdeb0059a935
Eniitan <enitanolawale0@gmail.com>
2024-04-14 15:42:56 +0100

    Sections can now be created

--- src/components/homepage/section/addSectionForm.jsx
+++ src/components/homepage/section/addSectionForm.jsx
@@ -1,16 +1,54 @@
+import { useState } from "react";
+import { useList } from "../../../hooks/stateProvider";
+
 export const AddSectionForm = () => {
+  const [title, setTitle] = useState("");
+  const { setProjects, projects, activeProject } = useList();
+
   const hideForm = () => {
     setShowForm(false);
   };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    const data2submit = {
+      title: form.get("section"),
+      parentProject: projects[activeProject]._id
+    };
+
+    try {
+      const res = await fetch("/api/section/create", {
+        method: "POST",
+        body: JSON.stringify(data2submit),
+        headers: { "Content-Type": "application/json" }
+      });
+
+      const response = await res.json();
+
+      console.log(response);
+     
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="ml-[4rem] w-[57%] border mt-5 p-4 rounded-xl">
+    <form
+      onSubmit={submitHandler}
+      className="ml-[4rem] w-[57%] border mt-5 p-4 rounded-xl">
       <input
         className="outline-none"
         type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
         placeholder="enter section name"
         name="section"
         required
+        autoFocus
       />
       <div className="w-fit ml-auto">
         <button
