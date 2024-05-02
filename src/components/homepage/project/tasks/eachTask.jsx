4d09c784a147917f1456406aa3d8d706f1093cdb
Eniitan <enitanolawale0@gmail.com>
2024-05-02 01:16:32 +0100

    implemented a sound effect for when users complete a task

--- src/components/homepage/project/tasks/eachTask.jsx
+++ src/components/homepage/project/tasks/eachTask.jsx
@@ -6,20 +6,31 @@ import { TaskEditForm } from "./taskEditForm";
 import { Options } from "./options";
 import { DueDate } from "./dueDate";
 import { useList } from "../../../../hooks/stateProvider";
+import soundEffect from "../../../../soundEffects/tap-notification-180637.mp3";
 
 export const EachTask = ({ task }) => {
   const [showOptions, setShowOptions] = useState(false);
   const [showDeleteWarning, setShowDeleteWarning] = useState(false);
   const [editTask, setEditTask] = useState(false);
+  const [popAndFade, setPopAndFade] = useState(false);
   const { fetchFunc, setError } = useList();
 
+  const playAudio = () => {
+    new Audio(soundEffect).play();
+  };
+
   const completeOrDelete = () => {
-    try {
-      fetchFunc("/api/task/delete?_method=DELETE", { taskId: task._id });
-    } catch (err) {
-      setError("There was an error, please try again");
-      console.error(err);
-    }
+    setPopAndFade(true);
+    setTimeout(() => {
+      try {
+        fetchFunc("/api/task/delete?_method=DELETE", { taskId: task._id });
+        playAudio();
+        setPopAndFade(false);
+      } catch (err) {
+        setError("There was an error, please try again");
+        console.error(err);
+      }
+    }, 200);
   };
 
   if (editTask) {
@@ -41,7 +52,9 @@ export const EachTask = ({ task }) => {
       onMouseLeave={() => {
         setShowOptions(false);
       }}
-      className="w-[90%] flex flex-row mt-7 border-b">
+      className={`w-[90%] flex flex-row mt-7 border-b ${
+        popAndFade && "growAndFade"
+      }`}>
       <CheckMarkButton
         clickFunc={completeOrDelete}
         additionalStyling={`${!task.dueDate && "mb-2"}`}
